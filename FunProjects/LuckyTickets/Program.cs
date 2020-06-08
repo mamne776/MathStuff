8bffec985c886aef6bc65e006492243770b16f30
mamne776 <mamne776@gmail.com>
2020-06-08 23:11:30 +0300

    LT begins
    Lucky Tickets

---
+++ FunProjects/LuckyTickets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuckyTickets
+{
+    class LuckyTickets
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n:D");
+        }
+    }
+}
